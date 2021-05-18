50487b608e6b395f0b788b32434756f19cc6cb68
mrbhaskar <reach.bhaskarmitra@gmail.com>
2021-05-18 23:58:56 +0530

    minor changes

--- src/app/pages/base/base.module.ts
+++ src/app/pages/base/base.module.ts
@@ -3,12 +3,11 @@ import { CommonModule } from '@angular/common';
 
 import { BaseRoutingModule } from './base-routing.module';
 import { BaseComponent } from './base.component';
-import { ProductCardComponent } from 'src/app/components/product-card/product-card.component';
 
 import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
-  declarations: [BaseComponent, ProductCardComponent],
+  declarations: [BaseComponent],
   imports: [CommonModule, BaseRoutingModule, MatCardModule],
 })
 export class BaseModule {}
